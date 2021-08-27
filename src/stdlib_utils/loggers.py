7b3db5b4584e7988a16e75ba0e92da6281b5f94b
Tanner Peterson <tanner@curibio.com>
2021-08-27 17:34:28 +0000

    added ability to set formatter of handlers in configure_logging

--- src/stdlib_utils/loggers.py
+++ src/stdlib_utils/loggers.py
@@ -21,6 +21,7 @@ def configure_logging(
     log_file_prefix: Optional[str] = None,
     log_level: int = logging.INFO,
     logging_format: str = "standard",
+    logging_formatter: Optional[logging.Formatter] = None,
 ) -> None:
     """Apply standard configuration to logging.
 
@@ -29,6 +30,7 @@ def configure_logging(
         log_file_prefix: if set without path_to_log_folder specified, will write logs to file in a subfolder (logs). By default it will create a subfolder in the current working directory (if running from source) or in the path that the EXE was installed to for pyinstaller. If path_to_log_folder is specified will write logs to file in the given log folder using this as the prefix.
         log_level: set the desired logging threshold level
         logging_format: the desired format of logging output. 'standard' should be used in all cases except for when used in a notebook.
+        logging_formatter: optional custom formatter to set on each logging handler. Useful as a catch-all in situations where information must be redacted from log files.
     """
     logging.Formatter.converter = time.gmtime  # ensure all logging timestamps are UTC
     stdout_handler = logging.StreamHandler(sys.stdout)
@@ -59,6 +61,11 @@ def configure_logging(
         config_format = "[%(asctime)s UTC] %(levelname)s - %(message)s"
     else:
         raise UnrecognizedLoggingFormatError(logging_format)
+
+    if logging_formatter is not None:
+        for handler in handlers:
+            handler.setFormatter(logging_formatter)
+
     logging.basicConfig(
         level=log_level,
         format=config_format,
